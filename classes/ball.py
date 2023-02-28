889ef23a74c55d666236e97b01fb248c11c06694
eko-develops <ekodevelops@gmail.com>
2023-02-27 23:53:07 -0800

    ball bounces off screen

--- classes/ball.py
+++ classes/ball.py
@@ -2,9 +2,12 @@ import pygame
 
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos, size):
+    def __init__(self, pong, x_pos, y_pos, size):
+        self.pong = pong
         self.x_pos = x_pos
+        self.x_dir = 1
         self.y_pos = y_pos
+        self.y_dir = 1
         self.speed = 10
         self.size = size
         self.colour = (255, 255, 255)
@@ -14,6 +17,21 @@ class Ball(pygame.sprite.Sprite):
         pygame.draw.circle(surface, self.colour, (self.x_pos, self.y_pos), self.size)
 
     def update(self):
+        max_height = self.pong.screen_rect.height
+        max_width = self.pong.screen_rect.width
+        print((self.x_pos, self.y_pos))
         if self.just_spawned is True:
-            self.y_pos += self.speed
+            self.y_pos = self.y_pos + (self.y_dir * self.speed)
+            self.x_pos = self.x_pos + (self.x_dir * self.speed)
             self.just_spawned = False
+        elif self.x_pos + self.size >= max_width:
+            self.x_dir = -1
+        elif self.x_pos - self.size <= 0:
+            self.x_dir = 1
+        elif self.y_pos + self.size >= max_height:
+            self.y_dir = -1
+        elif self.y_pos - self.size <= 0:
+            self.y_dir = 1
+
+        self.x_pos = self.x_pos + (self.x_dir * self.speed)
+        self.y_pos = self.y_pos + (self.y_dir * self.speed)
